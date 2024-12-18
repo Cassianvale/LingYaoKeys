940c5b8dc95a9b375030cacd65589f7100d9fd78
Cassianvale <1544257291@qq.com>
2024-12-18 23:38:49 +0800

    feat: 优化输入框焦点管理

---
+++ Converters/ViewModelToHotkeyStatusConverter.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Globalization;
+using System.Windows.Data;
+using WpfApp.ViewModels;
+
+namespace WpfApp.Converters
+{
+    public class ViewModelToHotkeyStatusConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is KeyMappingViewModel keyMappingVM)
+            {
+                return keyMappingVM.HotkeyStatus;
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+} 
